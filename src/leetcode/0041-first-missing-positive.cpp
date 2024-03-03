934cd7bd323c317171cf38de502e7aa1e607db30
Andrew Ashworth <andrew@ashworth.us>
2024-03-03 16:01:30 +0100

    Add cycle sort problems.

--- src/leetcode/0041-first-missing-positive.cpp
+++ src/leetcode/0041-first-missing-positive.cpp
@@ -1,46 +1,54 @@
 #include <bits/stdc++.h>
 #include <doctest/doctest.h>
+#include <io.h>
 
 using namespace std;
 
-// hard difficulty problem
-
-// requires knowing cycle sort, a quite esoteric sorting algorithm that
-// works in place and in linear time, but only with very hard to satisfy prereqs
-// # of distinct values < length of input
-// you know how many of each value are present
-
-// for this problem, we have to find the smallest positive integer not in the
-// array. this integer must lie in the range [1..N], where N is the length of
-// the input array.
-
-int fmp(vector<int> & nums)
+int fmp(vector<int> & n)
 {
-	int x = numeric_limits<int>::max();
-
-	for (auto n : nums) {
-		if (n >= 0) {
-			x = min(n, x);
+	// modified cycle sort
+	size_t N = n.size();
+	for (int i = 1; i <= N; i++) {
+		while (i != n[i - 1]) {
+			int j = n[i-1];
+			if (1 > j || j >= N || j == n[j - 1])
+				break;
+			swap(n[j - 1], n[i-1]);
 		}
 	}
 
-	return -1;
+	for (int i = 0; i < N; ++i) {
+		if (n[i] != i + 1) {
+			return i + 1;
+		}
+	}
+
+	return N + 1;
 }
 
-TEST_CASE("Example 1")
+TEST_SUITE("0041-first-missing-positive")
 {
-	vector<int> nums = { 1, 2, 0 };
-	REQUIRE_EQ(fmp(nums), 3);
-}
+	TEST_CASE("Example 1")
+	{
+		vector<int> nums = { 1, 2, 0 };
+		REQUIRE_EQ(fmp(nums), 3);
+	}
 
-TEST_CASE("Example 2")
-{
-	vector<int> nums = { 3, 4, -1, 1 };
-	REQUIRE_EQ(fmp(nums), 2);
-}
+	TEST_CASE("Example 2")
+	{
+		vector<int> nums = { 3, 4, -1, 1 };
+		REQUIRE_EQ(fmp(nums), 2);
+	}
 
-TEST_CASE("Example 1")
-{
-	vector<int> nums = { 7, 8, 9, 11, 12 };
-	REQUIRE_EQ(fmp(nums), 1);
+	TEST_CASE("Example 3")
+	{
+		vector<int> nums = { 7, 8, 9, 11, 12 };
+		REQUIRE_EQ(fmp(nums), 1);
+	}
+
+	TEST_CASE("Singleton")
+	{
+		vector<int> nums = { 1 };
+		REQUIRE_EQ(fmp(nums), 2);
+	}
 }
--- src/leetcode/0448-find-disappeared-numbers.cpp
+++ src/leetcode/0448-find-disappeared-numbers.cpp
@@ -5,21 +5,40 @@ using namespace std;
 
 // see related 0041 & 0268
 
-vector<int> fdn(vector<int> & nums)
+vector<int> fdn(vector<int> & n)
 {
-	for (int i = 0; i < nums.size(); ++i) {
-		while (i != nums[i]) { }
+	// modified cycle sort
+	size_t N = n.size();
+	for (int i = 1; i <= N; i++) {
+		while (i != n[i - 1]) {
+			if (n[i - 1] > N || n[n[i - 1] - 1] == n[i - 1])
+				break;
+			swap(n[n[i - 1] - 1], n[i - 1]);
+		}
 	}
+
+	vector<int> r {};
+	r.reserve(8);
+	for (int i = 0; i < N; i++) {
+		int j = i + 1;
+		if (j != n[i]) {
+			r.push_back(j);
+		}
+	}
+	return r;
 }
 
-TEST_CASE("Example 1")
+TEST_SUITE("0448-find-disappeared-numbers")
 {
-	vector<int> nums = { 4, 3, 2, 7, 8, 2, 3, 1 };
-	REQUIRE(ranges::equal(fdn(nums), vector<int> { 5, 6 }));
-}
+	TEST_CASE("Example 1")
+	{
+		vector<int> nums = { 4, 3, 2, 7, 8, 2, 3, 1 };
+		REQUIRE(ranges::equal(fdn(nums), vector<int> { 5, 6 }));
+	}
 
-TEST_CASE("Example 2")
-{
-	vector<int> nums = { 1, 1 };
-	REQUIRE(ranges::equal(fdn(nums), vector<int> { 2 }));
+	TEST_CASE("Example 2")
+	{
+		vector<int> nums = { 1, 1 };
+		REQUIRE(ranges::equal(fdn(nums), vector<int> { 2 }));
+	}
 }
