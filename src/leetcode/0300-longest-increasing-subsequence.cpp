626534de87cceafc6579d3f3f7991aa301f9ecba
Andrew Ashworth <andrew@ashworth.us>
2024-04-23 16:51:58 -0400

    Update 0300-LIS for gtest.

--- src/leetcode/0300-longest-increasing-subsequence.cpp
+++ src/leetcode/0300-longest-increasing-subsequence.cpp
@@ -1,33 +1,28 @@
-#include <doctest/doctest.h>
+#include <gtest/gtest.h>
 
 #include <vector>
 
 using namespace std;
 
-// Given an integer array A, return the length of the longest strictly
-// increasing subsequence.
-
-int lis(vector<int> const & a)
+// # Longest Increasing Subsequence
+// ## Recurrence
+// Let d[i] be the length of the LIS that ends at i, including i.
+int longest_increasing_subsequence_dp(vector<int> const & a)
 {
-	size_t const n = a.size();
-	vector<int> d(n, 1);
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < i; ++j) {
-			if (a[j] < a[i]) {
+	vector<int> d(a.size(), 1);
+	int best = 1;
+	for (int i = 1; i < d.size(); ++i) {
+		for (int j = i - 1; j >= 0; --j) {
+			if (a[i] > a[j])
 				d[i] = max(d[i], d[j] + 1);
-			}
 		}
+		best = max(best, d[i]);
 	}
-
-	int ans = d[0];
-	for (int i = 1; i < n; i++) {
-		ans = max(ans, d[i]);
-	}
-	return ans;
+	return best;
 }
 
-TEST_CASE("Example 1")
+TEST(LC0300, Example1)
 {
 	vector<int> const input { 10, 9, 2, 5, 3, 7, 101, 18 };
-	REQUIRE_EQ(lis(input), 4);
+	EXPECT_EQ(longest_increasing_subsequence_dp(input), 4);
 }
